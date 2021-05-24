ffacde1193280f2ead4ff7563e63ee9c433b1b76
Keoni Gandall <koeng101@gmail.com>
2021-05-23 21:09:26 -0700

    init of allbase

---
+++ models.go
@@ -0,0 +1,151 @@
+package biogolem
+
+import (
+	"database/sql"
+	"github.com/jmoiron/sqlx/types"
+)
+
+var schema = `
+
+-- Create Seqhash Table --
+CREATE TABLE seqhash (
+	seqhash TEXT PRIMARY KEY,
+	sequence TEXT NOT NULL,
+	circular BOOLEAN NOT NULL DEFAULT FALSE,
+	doublestranded BOOLEAN NOT NULL DEFAULT TRUE,
+	seqhashtype TEXT NOT NULL CHECK (seqhashtype IN ('DNA', 'RNA', 'PROTEIN')),
+	translation TEXT REFERENCES seqhash(seqhash)
+);
+
+-- Create Genbank Table --
+CREATE TABLE genbank (
+	accession TEXT PRIMARY KEY,
+	polyversion TEXT NOT NULL,
+	genbankhash TEXT NOT NULL, -- adler32 checksum
+	genbank TEXT NOT NULL, 
+	seqhash TEXT NOT NULL REFERENCES seqhash(seqhash)
+);
+
+-- Create Genbank Features Table --
+CREATE TABLE genbankfeatures (
+	seqhash TEXT NOT NULL REFERENCES seqhash(seqhash),
+	genbank TEXT NOT NULL REFERENCES genbank(id)
+);
+
+-- Create Uniprot Table --
+CREATE TABLE uniprot (
+	accession TEXT PRIMARY KEY,
+	uniprothash TEXT NOT NULL, -- adler32 checksum
+	uniprot JSON NOT NULL,
+	seqhash TEXT NOT NULL REFERENCES seqhash(seqhash)
+);
+
+-- Rhea
+
+CREATE TABLE IF NOT EXISTS chebi (
+        accession TEXT PRIMARY KEY,
+        subclassof TEXT REFERENCES chebi(accession)
+);
+
+CREATE TABLE IF NOT EXISTS compound (
+        id INT,
+        accession TEXT PRIMARY KEY,
+        position TEXT,
+        name TEXT,
+        htmlname TEXT,
+        formula TEXT,
+        charge TEXT,
+        chebi TEXT REFERENCES chebi(accession),
+        polymerizationindex TEXT,
+        compoundtype TEXT NOT NULL CHECK(compoundtype IN ('SmallMolecule', 'Polymer', 'GenericPolypeptide', 'GenericPolynucleotide', 'GenericHeteropolysaccharide'))
+);
+
+CREATE TABLE IF NOT EXISTS reactivepart (
+        id INT,
+        accession TEXT PRIMARY KEY,
+        name TEXT,
+        htmlname TEXT,
+        compound TEXT NOT NULL REFERENCES compound(accession)
+);
+
+CREATE TABLE IF NOT EXISTS reaction (
+        id INT,
+        directional BOOL NOT NULL DEFAULT false,
+        accession TEXT PRIMARY KEY,
+        status TEXT,
+        comment TEXT,
+        equation TEXT,
+        htmlequation TEXT,
+        ischemicallybalanced BOOL NOT NULL DEFAULT true,
+        istransport BOOL NOT NULL DEFAULT false,
+        ec TEXT,
+        location TEXT
+);
+
+CREATE TABLE IF NOT EXISTS reactionside (
+        accession TEXT PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS reactionsidereaction (
+        reaction TEXT NOT NULL REFERENCES reaction(accession),
+        reactionside TEXT NOT NULL REFERENCES reactionside(accession),
+        reactionsidereactiontype TEXT NOT NULL CHECK(reactionsidereactiontype IN ('substrateorproduct', 'substrate', 'product'))
+);
+
+CREATE TABLE IF NOT EXISTS reactionparticipant (
+        compound TEXT REFERENCES compound(accession),
+        reactionside TEXT NOT NULL REFERENCES reactionside(accession),
+        contains INTEGER,
+        containsn BOOL NOT NULL DEFAULT false,
+        minus BOOL NOT NULL DEFAULT false,
+        plus BOOL NOT NULL DEFAULT false
+);
+
+-- Uniprot to reaction
+
+CREATE TABLE IF NOT EXISTS uniprot_to_reaction (
+        reaction TEXT REFERENCES reaction(accession),
+        uniprot TEXT REFERENCES uniprot(accession)
+);
+`
+
+/******************************************************************************
+
+Schema structs begin here
+
+******************************************************************************/
+
+type Seqhash struct {
+	Seqhash        string         `db:"seqhash"`
+	Circular       bool           `db:"circular"`
+	DoubleStranded bool           `db:"doublestranded"`
+	Sequence       string         `db:"sequence"`
+	SeqhashType    string         `db:"seqhashtype"`
+	Translation    sql.NullString `db:"translation"`
+}
+
+type Genbank struct {
+	Id          string `db:"id"`
+	Genbank     string `db:"genbank"`
+	GenbankHash string `db:"genbankhash"`
+	PolyVersion string `db:"polyversion"`
+	Seqhash     string `db:"seqhash"`
+}
+
+type GenbankFeature struct {
+	Seqhash string `db:"seqhash"`
+	Genbank string `db:"genbank"`
+}
+
+type Uniprot struct {
+	Accession   string         `db:"accession"`
+	UniprotHash string         `db:"uniprothash"`
+	Uniprot     types.JSONText `db:"uniprot"`
+	Seqhash     string         `db:"seqhash"`
+}
+
+type GenbankFull struct {
+	Seqhashes       []Seqhash
+	Genbank         Genbank
+	GenbankFeatures []GenbankFeature
+}
---
+++ models_test.go
@@ -0,0 +1,91 @@
+package biogolem
+
+import (
+	"context"
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/ory/dockertest/v3"
+	dc "github.com/ory/dockertest/v3/docker"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+)
+
+var db *sqlx.DB
+var minioClient *minio.Client
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	options := &dockertest.RunOptions{
+		Repository: "minio/minio",
+		Tag:        "latest",
+		Cmd:        []string{"server", "/data"},
+		PortBindings: map[dc.Port][]dc.PortBinding{
+			"9000/tcp": []dc.PortBinding{{HostPort: "9000"}},
+		},
+		Env: []string{"MINIO_ACCESS_KEY=MYACCESSKEY", "MINIO_SECRET_KEY=MYSECRETKEY"},
+	}
+
+	resource, err := pool.RunWithOptions(options)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	endpoint := fmt.Sprintf("localhost:%s", resource.GetPort("9000/tcp"))
+	// or you could use the following, because we mapped the port 9000 to the port 9000 on the host
+	// endpoint := "localhost:9000"
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	// the minio client does not do service discovery for you (i.e. it does not check if connection can be established), so we have to use the health check
+	if err := pool.Retry(func() error {
+		url := fmt.Sprintf("http://%s/minio/health/live", endpoint)
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("status code not OK")
+		}
+		return nil
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	// now we can instantiate minio client
+	minioClient, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4("MYACCESSKEY", "MYSECRETKEY", ""),
+		Secure: false,
+	})
+	if err != nil {
+		log.Println("Failed to create minio client:", err)
+	}
+
+	// now we can use the client, for example, to list the buckets
+	_, err = minioClient.ListBuckets(context.Background())
+	if err != nil {
+		log.Fatalf("error while listing buckets: %v", err)
+	}
+
+	// Shows buckets are set up
+
+	// When you're done, kill and remove the container
+	if err = pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	//// You can't defer this because os.Exit doesn't care for defer
+	//if err := pool.Purge(resource); err != nil {
+	//	log.Fatalf("Could not purge resource: %s", err)
+	//}
+
+	os.Exit(code)
+}
