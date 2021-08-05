82631f5132be2442a53342bf861674524f6c06a9
Timothy Stiles <tim@stiles.io>
2021-08-04 19:42:53 -0700

    created getTarballFile function.

--- cmd/download.go
+++ cmd/download.go
@@ -65,7 +65,7 @@ func getChembl() error {
 
 	// find the sqlite file link
 	for _, link := range links {
-		// if it's a sqlite tarball save it's link
+		// if it's a sqlite tarball save its link
 		if strings.Contains(link, "sqlite.tar.gz") {
 			sqliteFileLink = link
 			break
@@ -74,7 +74,7 @@ func getChembl() error {
 
 	// if we didn't find it, bail.
 	if sqliteFileLink == "" {
-		log.Fatal("could not find sqlite file")
+		log.Fatal("could not find sqlite file link")
 	}
 
 	// get the tarball from the server that contains the sqlite file
@@ -89,48 +89,9 @@ func getChembl() error {
 		log.Fatalf("status code error: %d %s", response.StatusCode, response.Status)
 	}
 
-	// unzip the tarball
-	tarball, err := gzip.NewReader(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer tarball.Close()
+	// extra our sqlite file from the tarball and write to disk
+	err = getTarballFile(response.Body, ".db", "../data/build/chembl")
 
-	// create a new tarball reader to iterate through like a directory
-	directory := tar.NewReader(tarball)
-	var filename string // will save the filename of the file we're writing
-	// iterate through the tarball and save sqlite file name.
-	for {
-		header, err := directory.Next() // this creates a side effect that we'll exploit outside of this loop to actually save the file
-
-		if err == io.EOF { // this is the signal that we're done if we haven't already found the file we're looking for
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if filepath.Ext(header.Name) == ".db" { // assuming that our CHEMBL tarball will only contain one db.
-			filename = header.Name
-			break
-		}
-	}
-
-	// if the db exists write to disk
-	if filename != "" {
-		// write disk to file
-		file, err := os.Create(filepath.Join("../data/build", filename))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		// copy the uncompressed file to disk
-		if _, err := io.Copy(file, directory); err != nil { // that side effect I mentioned in the above for loop makes this possible to do out of loop.
-			log.Fatal(err)
-		}
-	}
 	return err
 }
 
@@ -249,4 +210,55 @@ func getPageLinks(url string) ([]string, error) {
 		}
 	})
 	return links, err
+}
+
+func getTarballFile(responseBody io.ReadCloser, fileNamePattern string, writePath string) error {
+	// unzip the tarball
+	tarball, err := gzip.NewReader(responseBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tarball.Close()
+	// create a new tarball reader to iterate through like a directory
+	directory := tar.NewReader(tarball)
+	var filename string // will save the filename of the file we're writing
+	// iterate through the tarball and save the file we're looking for.
+	for {
+		header, err := directory.Next() // this creates a side effect that we'll exploit outside of this loop to actually save the file
+		if err == io.EOF {              // this is the signal that we're done if we haven't already found the file we're looking for
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.Contains(header.Name, fileNamePattern) { // assuming that our tarball will only contain one file that will match our patten.
+			filename = header.Name
+			break
+		}
+	}
+
+	// if the file exists write to disk
+	if filename != "" {
+
+		// if the filepath does not exist, create it
+		err = os.MkdirAll(writePath, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// create empty file to write to
+		file, err := os.Create(filepath.Join(writePath, filename))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer file.Close()
+
+		// copy the uncompressed file to disk
+		if _, err := io.Copy(file, directory); err != nil { // that side effect I mentioned in the above for loop makes this possible to do out of loop.
+			log.Fatal(err)
+		}
+	}
+	return err
 }
