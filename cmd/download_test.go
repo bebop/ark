61f42e6f7d7ad263767b58de58609e18a88f3c45
Timothy Stiles <tim@stiles.io>
2021-08-06 11:24:31 -0700

    grammar.

--- cmd/download_test.go
+++ cmd/download_test.go
@@ -10,8 +10,8 @@ import (
 /******************************************************************************
 So Download as a command is a total pain to test locally and needs a fancy big
 server deployment to be fully tested. That's in the works but for now I've
-just broken most of what's needed into helper functions that can be easily unit
-tested.
+just broken down most of what's needed into helper functions that can be easily
+unit tested.
 
 TTFN,
 Tim
