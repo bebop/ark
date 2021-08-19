0fc16890c95c0e344f500324abd39402f9948d09
jordan-strasser <86021979+jordan-strasser@users.noreply.github.com>
2021-08-19 14:11:58 -0700

    Update openalchemy.go

--- openalchemy.go
+++ openalchemy.go
@@ -1,144 +1,102 @@
 package main
 
+import "database/sql"
 
 // Authors: Jordan Strasser, David Lambert (SQL Hero)
 
 import (
 	"fmt"
-	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	_ "log"
 	_ "os"
-	"strings"
 )
-// equ, short for equation, is a struct intended to break up an equation string into its id, reactants and products
-type equ struct {
-	id int
-	reactants []string
-	products []string
-}
-
 
 func main() {
 
-	database, _ := sql.Open("sqlite3", "./allbase.db")
-	// get_pathways parameters: 1 - database, 2 - target compound, 3 - # of reaction steps (do not go beyond 6 for now or there will be too many paths) 
-	get_pathways(database, "calycosin", 5)
+	database, err := sql.Open("sqlite3", "./allbase.db")
+	if err != nil {
+		fmt.Println("Issue opening the database")
+	}
+	// target_id and levels should be modifiable
+	get_pathways(database, 10019, 5)
 
 
 }
-// Recursively searches throughout the database and fetches the pathways that lead to your target compound.
+// recursively searches throughout the database and fetches the pathways that lead to your target compound.
 // This query excludes any compound that occurs more than 100 times in nodes. That parameter can change,
 // but once we start including things like NADPH, ATP, H2O, we step into combinatorial explosions.
 // id_path shows you the chain of equations starting from the top, and path shows you the actual compoounds
 // that build up a path, which is usually just the most significant reactants and products.
-func get_pathways(database *sql.DB, compound_name string, levels int) {
-	paths_query :=  `
-	WITH not_one (x) AS ( SELECT DISTINCT reactant
-	FROM nodes
-	WHERE reactant IN (
-		Select reactant From (
-		SELECT count(*) as count, reactant
-	FROM nodes
-	GROUP BY reactant
-	ORDER BY count(*) desc)
-	Where count > 100)
-),
+func get_pathways(database *sql.DB, target_id int, levels int) {
+	paths_query :=  `WITH stitch AS ( 
+select a.accession, a.id as rxn_id,
+b.reactionside, 
+b.reactionsidereactiontype,
+c.compound,
+d.id as cmpd_id,
+d.name
+FROM reaction a
+inner join reactionsidereaction b on a.accession = b.reaction 
+inner join reactionparticipant c USING(reactionside)
+inner join compound d ON c.compound = d.accession
+where b.reactionsidereactiontype <> 'substrateorproduct'
+), 
+not_one(ids) AS (
+SELECT cmpd_id FROM ( 
+SELECT count(*) as count, cmpd_id
+	FROM stitch
+	GROUP BY cmpd_id
+	ORDER BY count(*) DESC )
+	WHERE count > 100 -- modify to filter out commonly available molecules
+	),
 	chain AS (
-		SELECT id, reactant, product, 0 as lvl
-	,product || ',' || reactant as path
-	, cast(id as text) as id_path
-	FROM nodes
-	WHERE product = %q
-	AND reactant NOT IN (SELECT x FROM not_one)
-	UNION ALL
-	SELECT n.id, n.reactant, n.product, lvl + 1 as lvl, chain.path ||
-		',' || n.reactant as path
-	, chain.id_path || ',' || cast(n.id as TEXT)  as  id_path
-	FROM nodes n
-	INNER JOIN chain ON n.product = chain.reactant
-	WHERE lvl < %d
-	AND n.reactant NOT IN (SELECT x FROM not_one)
-	AND n.product NOT IN (SELECT x FROM not_one)
-	AND INSTR(chain.path, n.reactant) = 0
-	)
-	select * from chain`
+select c.rxn_id, c.reactionsidereactiontype as type1, c.cmpd_id as prod_id, c.name as prod_name,
+d.reactionsidereactiontype as type2, d.cmpd_id as sub_id, d.name as sub_name, 0 as lvl, 
+c.name|| ',' ||d.name as name_path, 
+cast(c.rxn_id as text) as id_path
+from 
+stitch c 
+inner join stitch d on c.rxn_id = d.rxn_id 
+where c.cmpd_id = %d
+and sub_id NOT IN (SELECT ids from not_one)
+and type2 = 'substrate'
+and type1 <> 'substrate'
+UNION ALL 
+select e.rxn_id, e.reactionsidereactiontype as type1, e.cmpd_id as prod_id, e.name as prod_name,
+f.reactionsidereactiontype as type2, f.cmpd_id as sub_id, f.name as sub_name, chain.lvl + 1 as lvl,
+chain.name_path || ',' || f.name as name_path, 
+chain.id_path || ',' || cast(e.rxn_id as text) as id_path 
+from 
+stitch e
+inner join stitch f on e.rxn_id = f.rxn_id 
+inner join chain on e.cmpd_id = chain.sub_id
+where lvl < %d
+and f.reactionsidereactiontype = 'substrate'
+and e.reactionsidereactiontype <> 'substrate'
+and f.cmpd_id NOT IN (SELECT ids FROM not_one)
+and instr(chain.name_path, f.name) = 0
+) 
+select * from chain`
 
 		tx, err := database.Begin()
 		if err != nil {fmt.Println(err, "Problem loading database")}
 
-		rows, err := tx.Query(fmt.Sprintf(paths_query, compound_name, levels))
+		rows, err := tx.Query(fmt.Sprintf(paths_query, target_id, levels))
 		if err != nil { fmt.Println(err, "Something's up with the rows returned")}
-		var id int
-		var reactant string
-		var product string
+		var rxn_id int
+		var type1 string
+		var prod_id int
+		var prod_name string
+		var type2 string
+		var sub_id int
+		var sub_name string
 		var lvl int
-		var path string
+		var name_path string
 		var id_path string
 		for rows.Next() {
-			rows.Scan(&id, &reactant, &product, &lvl, &path, &id_path)
-			fmt.Println(id, reactant, product, lvl, path, id_path)
+			rows.Scan(&rxn_id, &type1, &prod_id, &prod_name, &type2, &sub_id, &sub_name, &lvl, &name_path, &id_path)
+			fmt.Println(rxn_id, type1, prod_id, prod_name, type2, sub_id, sub_name, lvl, name_path, id_path)
 
 		}
 
 }
-
-
-
-
-
-
-
-
-
-// takes an equation and splits it up into lists of reactants and products. Excludes bidirectional reactions
-// because in Rhea, for some reason, they include both the forward and backward reactions.
-func split_mols(id int, equation string) (chunk equ) {
-	chunk.id  = id
-	sides := strings.Split(equation, "=>")
-	chunk.reactants = append(chunk.reactants, strings.Split(sides[0], " + ")...)
-	chunk.products = append(chunk.products, strings.Split(sides[1], " + ")...)
-
-	return chunk
-}
-// run this function if you need to add or update the nodes table
-func add_nodes(database *sql.DB) {
-
-	tx, _ := database.Begin()
-	stmt, er := tx.Prepare("DROP TABLE IF EXISTS nodes")
-	if er != nil {
-		fmt.Println(er)
-	} else {
-		stmt.Exec()
-	}
-	statement, err := tx.Prepare("CREATE TABLE IF NOT EXISTS nodes (id INTEGER, reactant TEXT, product TEXT)")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		statement.Exec()
-	}
-	get_rxns := "SELECT id, equation FROM reaction WHERE directional = 1 AND trim(equation) != ''"
-	rows, _ := tx.Query(get_rxns)
-	var id int
-	var equation string
-	que := "INSERT INTO nodes (id, reactant, product) VALUES (?, ?, ?)"
-	for rows.Next() {
-		rows.Scan(&id, &equation)
-		unit := split_mols(id, equation)
-		for _, reactant := range unit.reactants {
-			for _, product := range unit.products {
-				statement, err = tx.Prepare(que)
-				if err != nil {
-					fmt.Println("could not insert: ", err)
-				} else {
-					statement.Exec(id, reactant, product)
-				}
-			}
-		}
-		}
-		err = tx.Commit()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
