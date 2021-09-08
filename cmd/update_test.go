15f9661f02a86eb1c2257462b0703ecbd2ad106c
Isaac Guerreiro <ig00114@saqueepague.com.br>
2021-09-07 21:35:27 -0300

    Go mod and test function updated

--- cmd/update_test.go
+++ cmd/update_test.go
@@ -6,20 +6,6 @@ import (
 	"net/http"
 )
 
-func ExampleCreateCheckSumFile() {
-	response, err := http.Get("https://ftp.expasy.org/databases/rhea/rdf/rhea.rdf.gz")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer response.Body.Close()
-
-	CreateCheckSumFile("../data/build", "rhea.rdf.gz", response.Body)
-	fmt.Println("Hello")
-	// Output: Hell
-}
-
 func ExampleIsEqualFiles() {
 	response, err := http.Get("https://ftp.expasy.org/databases/rhea/rdf/rhea.rdf.gz")
 
--- go.mod
+++ go.mod
@@ -22,4 +22,5 @@ require (
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	lukechampine.com/blake3 v1.0.0 // indirect
 )
