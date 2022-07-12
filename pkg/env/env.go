a051c615797d382ca84d46b8aec836e5f8ced5a9
Timothy Stiles <tim@stiles.io>
2022-07-12 16:31:24 -0700

    hopefully fixed path issue with github actions.

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -10,13 +10,21 @@ import (
 // RootPath returns the root directory of the project as an absolute path.
 func RootPath() string {
 	_, runfile, _, _ := runtime.Caller(0)
-	dir := path.Dir(runfile)
-	rootPath := "/"
-	for _, subString := range strings.Split(dir, "/") {
-		rootPath = filepath.Join(rootPath, subString)
-		if subString == "allbase" {
-			break
+	dir := filepath.Join("/", path.Dir(runfile))
+
+	splitPath := strings.Split(dir, "/")
+	var lastAllBaseIndex int
+
+	// get last slice element equal to "allbase" of splitPath
+	for index, substring := range splitPath {
+		if substring == "allbase" {
+			lastAllBaseIndex = index
 		}
 	}
+
+	splitRootPath := splitPath[:lastAllBaseIndex+1]
+	rootPathWithoutSlash := filepath.Join(splitRootPath...)
+	rootPath := path.Join("/", rootPathWithoutSlash)
+
 	return rootPath
 }
