88a66066de29ab1c97d8550b44b56ae53d9fbe7a
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-01-17 23:03:41 +0000

    Updated the server code to check if the database exists.

--- server.go
+++ server.go
@@ -9,17 +9,31 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/TimothyStiles/allbase/graph"
 	"github.com/TimothyStiles/allbase/parameters"
+	"github.com/TimothyStiles/allbase/pkg/retsynth"
 )
 
+// RunServerChecks runs all the checks to make sure the server is ready to run.
+func RunServerChecks() {
+	// Check if the retsynth database connection is valid
+	var _, err = retsynth.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// StartGraphQLServer starts the GraphQL server on the port specified in the environment variable PORT or 8080 if not specified.
 func StartGraphQLServer() {
-	port, ok := os.LookupEnv("PORT")
+
+	RunServerChecks()
+
+	port, ok := os.LookupEnv("ALLBASE_PORT")
 	if !ok {
 		port = parameters.DefaultServerPort
 	}
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/", playground.Handler("AllBase GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
