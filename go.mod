c7b6efb78cfcc0dd8fb499a7bc851d6802b9930b
Timothy Stiles <tim@stiles.io>
2023-09-08 13:50:59 -0700

    added basic postgres code to main.go

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/jmoiron/sqlx v1.3.4
+	github.com/lib/pq v1.2.1-0.20191011153232-f91d3411e481
 	github.com/mattn/go-sqlite3 v1.14.12
 )
-
-require github.com/lib/pq v1.2.1-0.20191011153232-f91d3411e481 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "127.0.0.1"
+	port     = 65318
+	user     = ""
+	password = ""
+	dbname   = ""
+)
+
 func main() {
-	// cmd.Execute()
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
